4b1cd992a441fa0fdee79d12a3af1936736b44ec
bob <bob@>
2022-11-13 11:21:04 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+function HomePage() {
+    const mensagem = "Bem vindo ao Aluratube";
+    const estilosDaHomePage = { backgroundColor: "red" };
+    return (
+
+        <div style={estilosDaHomePage}>{mensagem}</div>
+    );
+  }
+  export default HomePage
+
+  function Menu() {
+return{
+    <div>
+        Menu
+</div>  
+}
+
+  }
+
+  function Heard() {
+    return{
+        <div>
+        Heard
+        </div>   
+    }
+    
+      }
+
+      function timeline() {
+        return{
+            <div>
+                |timeline
+            </div>   
+        }
+        
+          }
